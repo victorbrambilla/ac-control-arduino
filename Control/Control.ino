d53dcb190bba6c475a8ca087de1abb30f92b0346
victor brambilla <88890771+victorbrambilla@users.noreply.github.com>
2022-11-15 11:23:21 -0400

    Update Control.ino

--- Control/Control.ino
+++ Control/Control.ino
@@ -14,30 +14,16 @@ uint16_t powerOff[351] = { 684, 17792, 3092, 8864, 616, 380, 614, 1382, 610, 386
 
 ESP8266WebServer server(80);
 const int led = 13;
+
 void handleRoot() {
-  digitalWrite(led, 1);
   server.send(200, "text/plain", "Hello from esp8266!");
-  digitalWrite(led, 0);
 }
 void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Foundnn";
-  message += "URI: ";
-  message += server.uri();
-  message += "nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "nArguments: ";
-  message += server.args();
-  message += "n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
+  server.send(404, "text/plain", "404: Not found");
 }
+
 void setup(void) {
   pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.println("");
